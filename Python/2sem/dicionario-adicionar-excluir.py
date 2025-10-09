63952a2d8e4d194b9d65894489c54404ad420a13
DudaAcy <110108327+MariaEduardaAcyole@users.noreply.github.com>
2025-10-09 14:37:41 -0300

    Update dicionario-adicionar-excluir.py

--- Python/2sem/dicionario-adicionar-excluir.py
+++ Python/2sem/dicionario-adicionar-excluir.py
@@ -1,26 +1,24 @@
-dicionario
-
 # Crie um dicionário para armazenar nomes e telefones. Permita que o usuário:
 # Adicion/e novos contatos
 # Consulte pelo nome
 # Exclua um contato
 dic = {}
-nome = input('nome: ')
-tel = input('tel: ')
+
 
 def adicionar_contato(nome):
     dic[nome] = tel
     print(nome)
 
-adicionar_contato('duda')
+nome = input('nome: ')
+tel = input('tel: ')
+adicionar_contato(nome, tel)
 print(dic)
-
 
 def consultaNome():
     nomeBusca =input('nome busca')
     print('nome busca: ', nomeBusca)
-    if nome == nomeBusca:
-        print('nome encontrado')
+    if nomeBusca in dic:
+        print('nome encontrado', {dic[nomeBusca]})
     else:
         print('nome nao encontrado')
     return nomeBusca
@@ -29,8 +27,18 @@ print(consultaNome())
 
 def excluirContato():
     nomeExcluir = input('nome exlcuir ')
-    del dic[nome]
+    if nomeExcluir in dic:
+        del dic[nomeExcluir]
+    else:
+        print('nao encontrado')
     return nomeExcluir
     
 print(excluirContato())
 print(dic)
+
+frase = input('frase:')
+
+divisao = frase.split()
+
+print('divisao: ', divisao)
+
