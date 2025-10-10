e2150ff9d6d2c02ebb0c74d9f59e161bf489e6b7
DudaAcy <110108327+MariaEduardaAcyole@users.noreply.github.com>
2025-10-10 11:11:57 -0300

    Update grafico.py

--- Python/2sem/grafico.py
+++ Python/2sem/grafico.py
@@ -1,26 +1,50 @@
+# Importa a biblioteca matplotlib, que serve pra desenhar gráficos.
 import matplotlib.pyplot as plt
+
 # a) Faça o gráfico da função y = x (faça com 1000 pontos entre -100 e 100)
 # b) Faça o gráfico da função y = x² (faça com 1000 pontos entre -100 e 100)
-#x = [i for i in range(-100,101)]
-##for i in range(-100,101):
-##    x.append(i)
-#y = [i**2 for i in range(-100,101)]
-#plt.plot(x,y, marker='o', linestyle='--')
-#plt.title('Título do gráfico')
-#plt.xlabel('Eixo x')
-#plt.ylabel('Eixo y')
-#plt.grid(True)
-#plt.show()
-# faça de forma automática um código que o usuário entra com a qtde de linhas
-# e colunas e vc faça um gráfico alternando as cores entre preto e branco
-# assuma que a qtde de colunas é impar
+
+#range(-100, 101) → cria os números de -100 até 100.
+x = [i for i in range(-100, 101)]
+y = [i for i in range(-100, 101)]
+
+#x e y são listas com os mesmos valores → então a linha será uma reta diagonal (porque y = x)
+
+#plt.plot(x, y) → desenha o gráfico ligando os pontos (x, y).
+plt.plot(x, y)
+
+#plt.show() → mostra o gráfico.
+plt.show()
+
+#isso é uma função linear.
+
+# GRAFICO DE Y = X²
+x = [i for i in range(-100, 101)]
+y = [i**2 for i in range(-100, 101)]
+plt.plot(x,y, marker='o', linestyle='--')
+plt.show()
+
+# Isso gera uma curva em forma de U (parábola).
+# isso é uma função quadrática.
+
+plt.title('Título do gráfico')
+plt.xlabel('Eixo x')
+plt.ylabel('Eixo y')
+plt.grid(True)
+plt.show()
+
 
 # Faça um degrade com as cores rgb na vertical e horizontal
+#Cada linha é uma "faixa" de pixels
 matriz_rgb = [
 	[[0, 0, 0], [127, 127, 127], [0, 0, 0]],
 	[[255, 255, 255], [0, 0, 0], [255, 255, 255]],
 	[[0, 0, 0], [255, 255, 255], [0, 0, 0]]
                  ]
+
+#Quadradinhos pretos e brancos alternados — tipo um tabuleiro de xadrez.
+plt.imshow(matriz_rgb)
+plt.show()
 
 matiz_rgb2 = []
 for j in range(256):
@@ -35,8 +59,7 @@ plt.axis('off')
 
 plt.show()
 
-
-# Plotando o gráfico
+# Pilotando o gráfico
 # marker='o' para usar círculos, linestyle='-' para usar uma linha sólida
 #plt.plot(x, y, marker='o', linestyle='-')
 #plt.title('Gráfico de Coordenadas x, y')
@@ -44,3 +67,8 @@ plt.show()
 #plt.ylabel('Eixo y')
 #plt.grid(True) # Adiciona uma grade ao gráfico
 #plt.show()
+
+palavras = ['casa', 'carro', 'bicicleta', 'carro','carro','bicicleta']
+
+for i in range(len(palavras)):
+    print(i)
